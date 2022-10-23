8a291f322fb0486b963b617c6a0a8c583d8aa22f
Lionel COTE <grcote7@gmail.com>
2022-10-23 13:39:53 +0200

    Set errueur 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.layout')
+
+@section('title')
+    Erreur
+@endsection
+
+@section('main')
+    Erreur 404
+@endsection
--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -6,7 +6,7 @@
 
 @section('main')
     <form action="{{ url('users') }}" method="POST">
-        {{-- @csrf --}}
+        @csrf
         <label for="nom">Entrez votre nom : </label>
         <input type="text" name="nom" id="nom">
         <input type="submit" value="Envoyer !">
