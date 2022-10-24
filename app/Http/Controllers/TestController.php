087ce906eb90854cb2a3dd50a3e0509b09f3cb95
Lionel COTE <grcote7@gmail.com>
2022-10-24 09:41:56 +0200

    Feat: Define public/storage/

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,10 +6,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
+
 class TestController extends Controller
 {
 	public function index()
 	{
+		// Storage::disk('local')->put('recettes.txt', 'Contenu du fichier');
+		Storage::disk('public')->put('recettes.txt', 'Contenu du fichier');
 		$data = config('view.paths');
 		return view('pages.test')->with('data', implode(',',$data));
 	}
