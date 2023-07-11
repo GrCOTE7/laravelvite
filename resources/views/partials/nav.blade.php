5cdaa5fb815d9536726fc2953c3289de178ee8e2
Lionel COTE <grcote7@gmail.com>
2023-07-11 09:38:22 +0200

    Fix: del repeated nav link

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,7 +3,6 @@
     <a href="/" class="@if (request()->routeIs('welcome')) active @endif">Home</a> |
     <a href="/article/5" class="@if (request()->routeIs('article')) active @endif">Article</a> |
     <a href="/facture/7" class="@if (request()->routeIs('facture')) active @endif">Facture</a> |
-    <a href="/t" class="@if (request()->routeIs('test')) active @endif">Test</a> |
     <a href="/users" class="@if (request()->routeIs('users')) active @endif">Compte</a> |
     <a href="/film" class="@if (request()->routeIs('film*')) active @endif">Film</a> |
     <a href="/photo" class="@if (Request::is('photo')) active @endif">Photo</a> |
