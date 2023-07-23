30d68165714fce20bb11bfa9147bdd49a2a0f09b
Lionel COTE <grcote7@gmail.com>
2023-07-23 11:02:04 +0200

    Feat: Add request route

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,10 +3,11 @@
     <a href="/" class="@if (request()->routeIs('welcome')) active @endif">Home</a> |
     <a href="/product/5" class="@if (request()->routeIs('article')) active @endif">Product</a> |
     <a href="/bill/7" class="@if (request()->routeIs('facture')) active @endif">Bill</a> |
-    <a href="/account" class="@if (request()->routeIs('account')) active @endif">Account</a> |
+    <a href="/account" class="@if (request()->routeIs('account')) active @endif">Account</a> | 
     <a href="/film" class="@if (request()->routeIs('film*')) active @endif">Film</a> |
     <a href="/photo" class="@if (Request::is('photo')) active @endif">Photo</a> |
     <a href="/contact" class="@if (Request::is('contact')) active @endif">Contact</a> |
+    <a href="/req?name=Lionel" class="@if (request()->routeIs('req')) active @endif">Request</a> |
     <a href="/api/films" class="@if (Request::is('api/films')) active @endif">Api</a><a href="/api/films/7" class="@if (Request::is('api/films/7')) active @endif">7</a> |
 
     <a href="/t" class="@if (Request::is('t')) active @endif">Test</a>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UsersController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,8 +27,16 @@ Route::get('bill/{n}', function ($n) {
 		->with('number', $n);
 })
 	->where('n', '[0-9]+')
-	->name('facture')
-	->middleware('auth');
+	->name('facture');
+// ->middleware('auth');
+
+Route::get('/req', function (Request $request) {
+	return [
+		'data'  => $request->input('naAme', 'Lio'),
+		'data2' => $request->all(),
+		'var'   => 'MaVar',
+	];
+});
 
 Route::get('t', [TestController::class, 'index'])
 	->name('test');
